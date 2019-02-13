d70b0825950e4cc1ec6cd3fbfc3220ba0d1a04e7
Alberto Miedes <albertomg994@gmail.com>
2019-02-13 17:15:30 +0100

    db:migrate > This extension is autoremoved. Maybe create it with a migration?

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 20180320220625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "decidim_accountability_results", id: :serial, force: :cascade do |t|
     t.jsonb "title"
