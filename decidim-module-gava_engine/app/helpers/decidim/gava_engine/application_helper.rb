69f1f384e8a3772860a3e986488074dd12059dd8
Alberto Miedes <albertomg994@gmail.com>
2019-12-17 11:53:16 +0100

    Create Gava engine

---
+++ decidim-module-gava_engine/app/helpers/decidim/gava_engine/application_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Decidim
+  module GavaEngine
+    # Custom helpers, scoped to the gava_engine engine.
+    #
+    module ApplicationHelper
+    end
+  end
+end
---
+++ decidim-module-gava_engine/lib/decidim/gava_engine.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "decidim/gava_engine/admin"
+require "decidim/gava_engine/engine"
+require "decidim/gava_engine/admin_engine"
+require "decidim/gava_engine/component"
+
+module Decidim
+  # This namespace holds the logic of the `GavaEngine` component. This component
+  # allows users to create gava_engine in a participatory space.
+  module GavaEngine
+  end
+end
