0559e0677c0efc5d1e8fc34a4daba47e7b1f370e
Alberto Miedes <albertomg994@gmail.com>
2019-12-17 12:36:13 +0100

    Move custom view to site engine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :prepend_organization_views
+
+  private
+
+  def prepend_organization_views
+    prepend_view_path "#{site_custom_engine}/app/custom_views" if site_custom_engine
+  end
+
+  def site_custom_engine
+    "decidim-module-gava_engine" if %w(gava.decidim.test gava.populate.tools participa.gavacitat.cat).include?(request.host)
+  end
 end
