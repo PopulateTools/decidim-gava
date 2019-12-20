ce17f1fa5baeeafa80ee2f14972d6e62f951e077
Alberto Miedes <albertomg994@gmail.com>
2019-12-20 10:05:23 +0100

    Assign site engine in middleware

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "../../decidim-module-gava_engine/lib/decidim/gava_engine"
-
 require_relative "../../decidim-module-uned_engine/lib/decidim/uned_engine"
-require_relative "../../decidim-module-uned_engine/app/services/sso_client"
 require_relative "../../decidim-module-uned_engine/app/helpers/decidim/uned_engine/application_helper"
 require_relative "../../decidim-module-uned_engine/lib/decidim/uned_engine/query_helper"
 
@@ -14,7 +11,6 @@ class ApplicationController < ActionController::Base
 
   include Decidim::UnedEngine::ApplicationHelper
 
-  before_action :set_site_engine
   before_action :run_engine_hooks
   before_action :prepend_organization_views
   before_action :check_uned_session
@@ -23,27 +19,18 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_site_engine
-    @site_engine = if host.include?("gava")
-                     Decidim::GavaEngine::GAVA_ENGINE_ID
-                   elsif host.include?("uned")
-                     Decidim::UnedEngine::UNED_ENGINE_ID
-                   end
+  def site_engine
+    request.env["site_engine"]
   end
 
   def run_engine_hooks
-    return unless @site_engine == Decidim::UnedEngine::UNED_ENGINE_ID
+    return unless site_engine == Decidim::UnedEngine::UNED_ENGINE_ID
 
-    if request.path.include?("/account/delete")
-      raise ActionController::RoutingError, "Not Found"
-    elsif request.path.include?("/users/sign_in")
-      redirect_to "#{Decidim::UnedEngine::SSOClient::SSO_URL}?URL=#{root_url}"
-    end
+    raise(ActionController::RoutingError, "Not Found") if request.path.include?("/account/delete")
   end
 
-
   def prepend_organization_views
-    prepend_view_path "#{@site_engine}/app/custom_views" if @site_engine
+    prepend_view_path "#{site_engine}/app/custom_views" if site_engine
   end
 
   def host
