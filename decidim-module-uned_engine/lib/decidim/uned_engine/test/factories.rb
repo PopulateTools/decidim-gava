e6fba2db8b74321a2fe01e52a27e8130205c2db0
Alberto Miedes <albertomg994@gmail.com>
2019-12-23 17:28:33 +0100

    Remove unrequired dependencies that break

--- decidim-module-uned_engine/lib/decidim/uned_engine/test/factories.rb
+++ decidim-module-uned_engine/lib/decidim/uned_engine/test/factories.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-require "decidim/core/test/factories"
+# TODO: breaks in staging
+# require "decidim/core/test/factories"
 
-FactoryBot.define do
-  factory :uned_engine_component, parent: :component do
-    name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :uned_engine).i18n_name }
-    manifest_name :uned_engine
-    participatory_space { create(:participatory_process, :with_steps) }
-  end
+# FactoryBot.define do
+#   factory :uned_engine_component, parent: :component do
+#     name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :uned_engine).i18n_name }
+#     manifest_name :uned_engine
+#     participatory_space { create(:participatory_process, :with_steps) }
+#   end
 
-  # Add engine factories here
-end
+#   # Add engine factories here
+# end
