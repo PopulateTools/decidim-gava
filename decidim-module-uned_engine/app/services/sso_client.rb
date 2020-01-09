1cd61fdec7d9d0541f404f6d2ceafe7f3a3bd78d
Alberto Miedes <albertomg994@gmail.com>
2020-01-09 13:21:37 +0100

    Add exception

--- decidim-module-uned_engine/app/services/sso_client.rb
+++ decidim-module-uned_engine/app/services/sso_client.rb
@@ -64,7 +64,7 @@ module Decidim
 
         def login_authorized?
           # HACK: force our test user to be authorized
-          return true if user_nickname == "palvarez128"
+          return true if user_nickname == "palvarez128" || user_nickname == "amiedes3"
 
           student? ? (success? && active?) : success?
         end
