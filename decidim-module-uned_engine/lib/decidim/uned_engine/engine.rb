d78c642e0ee689b8db1cb2234c3da3dd09f9edb7
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-10-28 17:57:10 +0200

    Remove references to assets from engines

--- decidim-module-uned_engine/lib/decidim/uned_engine/engine.rb
+++ decidim-module-uned_engine/lib/decidim/uned_engine/engine.rb
@@ -15,21 +15,6 @@ module Decidim
         # root to: "uned_engine#index"
         resources :fake_sessions, only: [:new]
       end
-
-      initializer "decidim_uned_engine.assets" do |app|
-        app.config.assets.precompile += %w(
-          decidim_uned_engine_manifest.js
-          decidim_uned_engine_manifest.scss
-          arrow-left.svg
-          arrow-left-bottom.svg
-          arrow-right.svg
-          arrow-right-bottom.svg
-          close.svg
-          logo-uned.png
-          uned-header-image.png
-          chevron-down-solid.svg
-        )
-      end
     end
   end
 end
