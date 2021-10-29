e1715c199750a024fbd2214e6f91821e7d04e2bd
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-10-29 11:26:15 +0200

    Configure AS services and set them on environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = :test
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
