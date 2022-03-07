295f8a5759fd5c57157697ba4426521cbb719703
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-03-07 20:10:57 +0100

    Remove numeric answers from question captcha configuration

--- config/initializers/decidim_questions.rb
+++ config/initializers/decidim_questions.rb
@@ -3,22 +3,22 @@
 Decidim::QuestionCaptcha.configure do |config|
   config.questions = {
       en: [
-          { "question" => "1+5", "answers" => "6,six" },
-          { "question" => "2+2", "answers" => "4,four" },
-          { "question" => "3+3", "answers" => "6,six" },
-          { "question" => "4+2", "answers" => "6,six" }
+          { "question" => "1+5", "answers" => "six" },
+          { "question" => "2+2", "answers" => "four" },
+          { "question" => "3+3", "answers" => "six" },
+          { "question" => "4+2", "answers" => "six" }
       ],
       es: [
-          { "question" => "0+0", "answers" => "0,zero" },
-          { "question" => "3+4", "answers" => "7,siete" },
-          { "question" => "4+4", "answers" => "8,ocho" },
-          { "question" => "2+2", "answers" => "4,cuatro" }
+          { "question" => "0+0", "answers" => "zero" },
+          { "question" => "3+4", "answers" => "siete" },
+          { "question" => "4+4", "answers" => "ocho" },
+          { "question" => "2+2", "answers" => "cuatro" }
       ],
       ca: [
-          { "question" => "8+1", "answers" => "9,nou" },
-          { "question" => "3+4", "answers" => "7,set" },
-          { "question" => "4+4", "answers" => "8,vuit" },
-          { "question" => "2+2", "answers" => "4,quatre" }
+          { "question" => "8+1", "answers" => "nou" },
+          { "question" => "3+4", "answers" => "set" },
+          { "question" => "4+4", "answers" => "vuit" },
+          { "question" => "2+2", "answers" => "quatre" }
       ]
   }
 end
